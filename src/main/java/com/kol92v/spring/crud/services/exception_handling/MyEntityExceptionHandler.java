956c7447949b0bb734ead77cc37b163482c43635
Ilya Pleshanov <kol92v@mail.ru>
2021-04-21 17:02:22 +0300

    add SQLIntegrityConstraintViolationException handling

--- src/main/java/com/kol92v/spring/crud/services/exception_handling/MyEntityExceptionHandler.java
+++ src/main/java/com/kol92v/spring/crud/services/exception_handling/MyEntityExceptionHandler.java
@@ -7,11 +7,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 @ControllerAdvice
 public class MyEntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<MyEntityIncorrectRequest> handleException(EntityRequestException exception) {
+        MyEntityIncorrectRequest myEntityIncorrectRequest = new MyEntityIncorrectRequest(exception.getMessage());
+        return new ResponseEntity<>(myEntityIncorrectRequest, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<MyEntityIncorrectRequest> handleException(SQLIntegrityConstraintViolationException exception) {
         MyEntityIncorrectRequest myEntityIncorrectRequest = new MyEntityIncorrectRequest(exception.getMessage());
         return new ResponseEntity<>(myEntityIncorrectRequest, HttpStatus.BAD_REQUEST);
     }
